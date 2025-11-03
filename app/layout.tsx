2694e9d17f9aae3ee29cba60568ad2c950d8af31
Saptak Manna <saptakmanna100@gmail.com>
2025-11-03 09:30:52 +0530

    Add subsets option to Poppins font configuration

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "@/styles/globals.css";
 
 const poppins = Poppins({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
