0b138890491c47489b3a2ffa88cafeeb783089ef
rahulc0dy <rc645312@gmail.com>
2025-11-22 13:07:00 +0530

    Fix default roadWidth value in World constructor from 8 to 40

--- lib/world.ts
+++ lib/world.ts
@@ -35,7 +35,7 @@ export class World {
   constructor(
     graph: Graph,
     scene: Scene,
-    roadWidth: number = 8,
+    roadWidth: number = 40,
     roadRoundness: number = 8
   ) {
     this.graph = graph;
